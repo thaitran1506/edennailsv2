723357ac39125aeb6be11a673545f53a66544338
thaitran1506 <45746360+thaitran1506@users.noreply.github.com>
2025-09-07 16:01:24 -0700

    Update layout.tsx
    add speed Insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Newsreader, Playfair_Display } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Header from "@/components/Header";
 import "./globals.css";
 
